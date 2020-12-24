cdc43c28bdbe97ace3f6a2e18315ac282e542804
Ratchanon38 <53069188+Ratchanon38@users.noreply.github.com>
2020-12-24 18:27:40 +0700

    Update done.vue

--- pages/register/done.vue
+++ pages/register/done.vue
@@ -81,8 +81,28 @@ export default {
       }      
     },
     close(){
-      liff.closeWindow();
+     liff.getProfile().then(function (profile) {
+	  liff.sendMessages([
+		{
+		  type: 'image',
+		  originalContentUrl: 'https://' + document.domain + '/imgs/' + res + '.jpg',
+		  previewImageUrl: 'https://' + document.domain + '/imgs/' + res + '_240.jpg'
+		},
+		{
+		  type: 'text',
+		  text: 'From:' + profile.displayName
+		}
+	  ]).then(function () {
+		liff.closeWindow();
+	  }).catch(function (error) {
+		window.alert('Error sending message: ' + error.message);
+	  });
+	}).catch(function (error) {
+		window.alert("Error getting profile: " + error.message);
+	});
+}
+  
     }
   }
-}
+
 </script>
