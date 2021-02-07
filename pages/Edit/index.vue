c58bc879fb802e31f8932e4363e58423a13a098e
Ratchanon38 <53069188+Ratchanon38@users.noreply.github.com>
2021-02-07 15:52:55 +0700

    Update index.vue

--- pages/Edit/index.vue
+++ pages/Edit/index.vue
@@ -177,7 +177,7 @@ export default {
     isDone() {
       this.$axios
         .get(
-          `https://chatbotlab101-78413-default-rtdb.firebaseio.com/members/${this.$store.getters.getLine.userId}/profile.json`
+          `https://u-bot-chat-kjhpyo.firebaseio.com/members/${this.$store.getters.getLine.userId}/profile.json`
         )
         .then((res) => {
           if (res.data != null) {
